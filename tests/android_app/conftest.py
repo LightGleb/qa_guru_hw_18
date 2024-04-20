872094f02cbaca9d59d5e91944e208d0c763f184
lightgleb <prorocker7@gmail.com>
2024-04-20 21:02:22 +0300

    1. Поправил browser.config.driver
    2. Прописал версии зависимостей
    
    3. Добавил видео и фото атачи
    
    4. Кредлы спрятал в .env

--- tests/android_app/conftest.py
+++ tests/android_app/conftest.py
@@ -1,11 +1,23 @@
 import pytest
 from appium.options.android import UiAutomator2Options
+from dotenv import load_dotenv
 from selene import browser
 import os
+
+from appium import webdriver
+
+from utils import attach
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def mobile_management():
+    login = os.getenv("LOGIN")
+    accesskey = os.getenv("ACCESS_KEY")
     options = UiAutomator2Options().load_capabilities({
         # Specify device and os_version for testing
         "platformName": "android",
@@ -22,16 +34,21 @@ def mobile_management():
             "sessionName": "BStack first_test",
 
             # Set your access credentials
-            "userName": "bsuser_kjLlYv",
-            "accessKey": "EvecjHUiAyAa5HUB9znF"
+            "userName": login,
+            "accessKey": accesskey
         }
     })
 
-    browser.config.driver_remote_url = 'http://hub.browserstack.com/wd/hub'
-    browser.config.driver_options = options
+    browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
 
     browser.config.timeout = float(os.getenv('timeout', '10.0'))
 
     yield
 
+    attach.add_screenshot(browser)
+    attach.add_xml(browser)
+    session_id = browser.driver.session_id
+
     browser.quit()
+
+    attach.add_video(session_id, login, accesskey)
